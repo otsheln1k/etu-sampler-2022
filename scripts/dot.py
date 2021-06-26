5601f6501b42630a28b70cc53e3a264a02ca6b30
Leonid Parfentiev <leo.parfentiev@gmail.com>
2021-06-26 15:23:47 +0300

    Add dot.py presenter script

---
+++ scripts/dot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+def ptitle(pt):
+    return f"{pt['file']}:{pt['line']} ({pt['func']})"
+
+def etitle(e):
+    t = e['total-usec']
+    c = e['count']
+    a = t / c
+    return f"{t}/{c} = {a}"
+
+def esc(s):
+    es = s.replace('\\', '\\\\').replace('"', '\\"')
+    return f'"{es}"'
+
+def wrgraph(nodelabels, edges,
+            file=sys.stdout):
+    fprint = lambda *a, **kwa: print(*a, file=file, **kwa)
+    fprint('digraph {')
+    for i, txt in enumerate(nodelabels):
+        fprint(f'  {i} [label={esc(txt)}]')
+    for i, j, txt in edges:
+        fprint(f'  {i} -> {j} [label={esc(txt)}]')
+    fprint('}')
+
+j = json.load(sys.stdin)
+ns = []
+es = []
+for i, pt in enumerate(j):
+    ns.append(ptitle(pt))
+    for e in pt['edges']:
+        es.append((i, e['dest'], etitle(e)))
+wrgraph(ns, es)
