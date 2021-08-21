35487fe9f398dc3ac92fec520db5c2cca3f23e8f
Leonid Parfentiev <leo.parfentiev@gmail.com>
2021-08-21 17:42:46 +0300

    scripts/nruns.sh: allow cmdline args, add simple skipping

--- scripts/nruns.sh
+++ scripts/nruns.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 
-if [ "$#" != 2 ]
+if [ "$#" -lt 3 ]
 then
-    echo "usage: $0 PROGRAM N" >&2
+    cat >&2 <<EOF
+usage: $0 N S PROGRAM [ARGS...]
+  N is the total number of runs.
+  S is the number of initial runs to skip.
+  PROGRAM is the program to run. It's always interpreted as a relative
+    path, and never searched in \$PATH.
+EOF
     exit 2
 fi
 
-program="$(dirname "$1")/$(basename "$1")"
-n="$2"
+n="$1"
+s="$2"
+program="$(dirname "$3")/$(basename "$3")"
+shift 3
+
+first="$(expr "$s" + 1)"
 
 echo '['
 seq "$n" | \
     while read i
     do
-        [ "$i" != 1 ] && echo ','
-        echo '['
-        "$program" -O 3 3>&1 >/dev/null | sed '1!s/^/,/'
-        echo ']'
+        if [ "$i" -le "$s" ]
+        then
+            "$program" -o /dev/null -- "$@" >/dev/null 2>&1
+        else
+            [ "$i" -ne "$first" ] && echo ','
+            echo '['
+            "$program" -O 3 -- "$@" 3>&1 >/dev/null | sed '1!s/^/,/'
+            echo ']'
+        fi
     done
 echo ']'
