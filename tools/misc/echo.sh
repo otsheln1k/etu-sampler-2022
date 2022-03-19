c7dae17a27ef9988ff330f2a47bd2239fb2254a8
Leonid Parfentiev <leo.parfentiev@gmail.com>
2022-03-19 19:02:27 +0300

    Remove old scripts

--- tools/misc/echo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-a="$1"
-b="$2"
-c="$3"
-n="$4"
-shift 4
-printf '%d  %s %s %s\n' $$ "$n" "$a" "$b"
-exec "$n" "$a" "$b" "$c" "$@"
--- tools/misc/ign.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo ign >&2
-printf '0 %s\n' "$0"
-printf '> %s\n' "$@" >&2
-shift 3
-n="$1"
-shift
-exec "$n" "$@"
