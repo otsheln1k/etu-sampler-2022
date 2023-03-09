3a6e85a6824b564c6b35c7858ee42bca76d82758
Leonid Parfentiev <leo.parfentiev@gmail.com>
2023-03-10 00:02:17 +0300

    Add simple.c test program

---
+++ programs/simple.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+#include "sampler.h"
+
+int min_element (int *arr, size_t n)
+{
+    SAMPLE;
+    if (n == 0) {
+        SAMPLE;
+        return 0;
+    }
+    SAMPLE;
+    int x = arr[0];
+    SAMPLE;
+    for (size_t i = 1; i < n; ++i) {
+        SAMPLE;
+        if (arr[i] < x) {
+            SAMPLE;
+            x = arr[i];
+            SAMPLE;
+        }
+        SAMPLE;
+    }
+    SAMPLE;
+    return x;
+}
+
+int main (int argc, char **argv)
+{
+    sampler_init(&argc, argv);
+
+    size_t n = 100;
+    int *arr = malloc(n * sizeof(*arr));
+
+    srand(0xDEADBEEF);
+    for (size_t i = 0; i < n; ++i) {
+        arr[i] = rand() % 5000;
+    }
+
+    int ans = min_element(arr, n);
+    (void)ans;
+
+    free(arr);
+
+    return 0;
+}
